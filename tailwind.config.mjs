3dcff740c61cba4faea707a08c1de8419c369345
mamadadapapamama <your.email@example.com>
2026-01-06 18:40:43 +0800

    fix: index

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -4,29 +4,17 @@ export default {
 	theme: {
 		extend: {
 			colors: {
-				primary: {
-					50: '#f0fdf4',
-					100: '#dcfce7',
-					200: '#bbf7d0',
-					300: '#86efac',
-					400: '#4ade80',
-					500: '#10b981', // Main green
-					600: '#059669',
-					700: '#047857',
-					800: '#065f46',
-					900: '#064e3b',
-				},
 				custom: {
 					brown: '#674949',
-					'bg-pink': '#FFFAF8',
-					'light-pink': '#FFEEE8',
-					'light-yellow': '#FFFDF4',
+					bgPink: '#FFFAF8',
+					lightPink: '#FFEEE8',
+					lightYellow: '#FFFDF4',
 					grey: '#D0CECE',
-					'dark-grey': '#6C6C6C',
+					darkGrey: '#6C6C6C',
 					rose: '#C64444',
 					green: '#227A71',
 					white: '#FFFFFF',
-					black: '#000000',
+					black: '#2B1111',
 				},
 			},
 			fontFamily: {
