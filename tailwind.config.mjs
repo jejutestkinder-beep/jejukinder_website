35774314f03604558ff2a613e18afd8933bd9fd6
mamadadapapamama <your.email@example.com>
2026-01-06 16:23:12 +0800

    update philosophy page, add animation effect on 2col component

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -33,6 +33,15 @@ export default {
 				heading: ['gaegu', 'cursive'], // h1, h2
 				body: ['roboto', 'sans-serif'], // h3, p, body
 			},
+			keyframes: {
+				'fade-in': {
+					'0%': { opacity: '0' },
+					'100%': { opacity: '1' },
+				},
+			},
+			animation: {
+				'fade-in': 'fade-in 0.6s ease-in-out forwards',
+			},
 		},
 	},
 	plugins: [],
