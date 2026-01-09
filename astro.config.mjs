1d0f6546e91244480d82dd3e0267edfd56025b56
mamadadapapamama <your.email@example.com>
2026-01-09 09:50:27 +0800

    add and change icons

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,8 +1,17 @@
 // @ts-check
 import { defineConfig } from 'astro/config';
 import tailwind from '@astrojs/tailwind';
+import netlify from '@astrojs/netlify';
+import icon from 'astro-icon';
 
 // https://astro.build/config
 export default defineConfig({
-	integrations: [tailwind()],
+	integrations: [
+		tailwind(),
+		icon({
+			iconDir: 'src/assets/icons'
+		})
+	], 
+	output: 'static', 
+	adapter: netlify(),
 });
