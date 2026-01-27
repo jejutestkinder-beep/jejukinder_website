1ced369eb17abe7b7e9a18028c7fcbd5c158c623
mamadadapapamama <your.email@example.com>
2026-01-28 00:57:56 +0800

    output static

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,16 +5,11 @@ import tailwind from '@astrojs/tailwind';
 // import netlify from '@astrojs/netlify'; // 1. Netlify는 사용하지 않으므로 주석 처리하거나 삭제하세요.
 import icon from 'astro-icon';
 import sitemap from '@astrojs/sitemap';
-import cloudflare from '@astrojs/cloudflare';
 
 // https://astro.build/config
 export default defineConfig({
     site: 'https://jejuwaldorf-kinder.com',
-    output: 'server', // server로 사용하고 prerender 로 제어한대
-    adapter: cloudflare({
-        // 2. 이미지 처리 방식을 'passthrough'로 설정하여 빌드 속도를 대폭 높입니다.
-        imageService: 'passthrough' 
-    }),
+    output: 'static',
     integrations: [
         tailwind(),
         icon({
